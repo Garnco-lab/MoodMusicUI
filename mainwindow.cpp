fc554aed959b6fadeb621136dc15adb40f78ef23
Steven <stevendeleonwork@gmaill.com>
2022-03-08 08:03:32 -0800

    updated main ui to link up with the file so it can change

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -16,26 +16,35 @@
 
 #include <string>
 
+#include <Windows.h>
+
 #include <stdlib.h>
 
 #include <stdio.h>
 
 #include <QFuture>
 
-class MyThread : public QThread {
-    void run() override {
-        system("cd C:\\Users\\Steven\\MoodMusic && python main.py");
-    }
+#include <winbase.h>
+
+#include <processthreadsapi.h>
+
+class MyThread: public QThread {
+  void run() override {
+
+    WinExec("python C:\\Users\\Steven\\MoodMusic", SW_SHOW);
+
+  }
 };
 
-
-
 MainWindow::MainWindow(QWidget * parent): QMainWindow(parent), ui(new Ui::MainWindow) {
-  ui -> setupUi(this);
-  this->setStyleSheet("background-color: black;");
 
-  int w = ui->label_pic->width();
-  int h = ui->label_pic->height();
+  QFile file("C:\\Users\\Steven\\MoodMusic\\music-value.txt");
+
+  ui -> setupUi(this);
+  this -> setStyleSheet("background-color: black;");
+
+  int w = ui -> label_pic -> width();
+  int h = ui -> label_pic -> height();
 
   QPixmap pix("C:/Users/Steven/Documents/MoodMusicUI/images/brain.png");
   ui -> label_pic -> setPixmap(pix.scaled(w, h, Qt::KeepAspectRatio));
@@ -43,11 +52,8 @@ MainWindow::MainWindow(QWidget * parent): QMainWindow(parent), ui(new Ui::MainWi
   ui -> progressBar -> setValue(ui -> horizontalSlider -> value());
   connect(ui -> horizontalSlider, SIGNAL(valueChanged(int)), ui -> progressBar, SLOT(setValue(int)));
 
-  MyThread *thread = new MyThread;
-  thread->start();
-//  QProcess process;
-//  process.start("cmd.exe", QStringList() << "C:\\Users\\Steven\\MoodMusic && python main.py");
-//  process.waitForFinished(-1);
+  MyThread * thread = new MyThread;
+  thread -> start();
 
 }
 
